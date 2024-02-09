ca08bb400c677889265674dd10c51416f10e82be
Likhan <iftekhar15-4243@diu.edu.bd>
2024-02-09 15:11:48 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const popup=document.querySelector('.popup')
+// const closeButton = document.querySelector('.close-btn')
+
+function closePopup() {
+    popup.classList.remove("active");
+} 
+function openPopup() {
+    popup.classList.add("active");
+} 
+
